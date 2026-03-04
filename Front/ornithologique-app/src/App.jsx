c6fa8fbdaa0c60972ff899b58f71cc98a8a7e9c9
Eliot De Lemos <eliot.delemos@gmail.com>
2026-03-04 19:35:14 +0100

    fix erreur

--- Front/ornithologique-app/src/App.jsx
+++ Front/ornithologique-app/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import HomePage from './pages/HomePage'
-import SpeciesListPage from './pages/SpeciesListPage'
-import SpeciesDetailPage from './pages/SpeciesDetailPage'
-import AddSpeciesPage from './pages/AddSpeciesPage'
-import TablePage from './pages/TablePage'
-import DetectionPage from './pages/DetectionPage'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import SpeciesListPage from "./pages/SpeciesListPage";
+import SpeciesDetailPage from "./pages/SpeciesDetailPage";
+import AddSpeciesPage from "./pages/AddSpeciesPage";
+import TablePage from "./pages/TablePage";
+import DetectionPage from "./pages/DetectionPage";
+import "./App.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home')
+  const [currentPage, setCurrentPage] = useState("home");
 
   const renderCurrentPage = () => {
-    if (currentPage === 'home') return <HomePage />
-    if (currentPage === 'list') return <SpeciesListPage />
-    if (currentPage === 'detail') return <SpeciesDetailPage />
-    if (currentPage === 'add') return <AddSpeciesPage />
-    if (currentPage === 'table') return <TablePage />
-    return <DetectionPage />
-  }
+    if (currentPage === "home") return <HomePage />;
+    if (currentPage === "list") return <SpeciesListPage />;
+    if (currentPage === "detail") return <SpeciesDetailPage />;
+    if (currentPage === "add") return <AddSpeciesPage />;
+    if (currentPage === "table") return <TablePage />;
+    return <DetectionPage />;
+  };
 
-function App() {
   return (
     <div className="app-layout">
       <Navbar currentPage={currentPage} onChangePage={setCurrentPage} />
       {renderCurrentPage()}
       <Footer />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
