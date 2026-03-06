7146bf8bac3e085126e8e4b6902f9d5e8102cfe2
Eliot De lemos <167034304+Alpha-NeMeSiS@users.noreply.github.com>
2026-03-06 17:58:16 +0100

    Update Footer.jsx

--- Front/ornithologique-app/src/components/Footer.jsx
+++ Front/ornithologique-app/src/components/Footer.jsx
@@ -9,15 +9,23 @@ function Footer() {
       </div>
       <div>
         <h4>Navigation</h4>
-        <p>Accueil</p>
-        <p>Liste des espèces</p>
-        <p>Dernières observations</p>
+         <Link to="/"
+           <p>Accueil</p>
+          </Link>
+          <Link to="/species"
+            <p>Liste des espèces</p>
       </div>
       <div>
         <h4>Outils</h4>
-        <p>Détection IA</p>
-        <p>Ajouter une observation</p>
-        <p>Exporter CSV</p>
+        <Link to="/detect"
+          <p>Détection IA</p>
+        </Link>
+        <Link to="/add"
+          <p>Ajouter une observation</p>
+        </Link>
+        <Link to="/table"
+          <p>Exporter CSV</p>
+        </Link>
       </div>
     </footer>
   )
