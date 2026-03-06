534df5ef8c3a320c6f2e0ffb430eca9c6873225f
Eliot De Lemos <eliot.delemos@gmail.com>
2026-03-06 18:21:10 +0100

    update footer

--- Front/ornithologique-app/src/components/Footer.jsx
+++ Front/ornithologique-app/src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import './Footer.css'
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -9,27 +10,18 @@ function Footer() {
       </div>
       <div>
         <h4>Navigation</h4>
-         <Link to="/">
-           Accueil
-          </Link>
-          <Link to="/species">
-            Liste des espèces
-          </Link>
+        <Link to="/">Accueil</Link>
+        <br />
+        <Link to="/species">Liste des espèces</Link>
       </div>
       <div>
         <h4>Outils</h4>
-        <Link to="/detect">
-          Détection IA
-        </Link>
-        <Link to="/add">
-          Ajouter une observation
-        </Link>
-        <Link to="/table">
-          Exporter CSV
-        </Link>
+        <Link to="/detect">Détection IA</Link><br />
+        <Link to="/add">Ajouter une observation</Link><br />
+        <Link to="/table">Exporter CSV</Link>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
